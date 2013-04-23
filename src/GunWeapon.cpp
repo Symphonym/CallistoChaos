2d59e327847e0cfd4f5727aa9922038a77715c39
Jakob Larsson <tidalage@gmail.com>
2013-04-23 15:39:32 +0200

    Just tested a new fire animation.

--- src/GunWeapon.cpp
+++ src/GunWeapon.cpp
@@ -12,8 +12,8 @@ GunWeapon::GunWeapon(const std::string &name, TileCharacter *tileCharacter, jl::
 		assets.getAsset<jl::TextureAsset>("res/weapons.png")->get(),
 		assets.getAsset<jl::TextureAsset>("res/bullets.png")->get());
 
-	addStance("right", sf::Vector2f(5,5), sf::IntRect(0,0,16,16), sf::Vector2f(6, -2.7), sf::Vector2f(7, -1.5));
-	addStance("left", sf::Vector2f(-5, 5), sf::IntRect(16,0,16,16), sf::Vector2f(-6, -2.7), sf::Vector2f(-4.5, 0));
+	addStance("right", sf::Vector2f(5,5), sf::IntRect(0,0,16,16), sf::Vector2f(6, -2.7), sf::Vector2f(6, -1.5));
+	addStance("left", sf::Vector2f(-5, 5), sf::IntRect(16,0,16,16), sf::Vector2f(-7, -2.7), sf::Vector2f(-4.5, 0));
 	addStance("up", sf::Vector2f(5, 5), sf::IntRect(32,0,16,16));
 	addStance("down", sf::Vector2f(0, 5), sf::IntRect(48,0,16,16), sf::Vector2f(0.5,0));
 	setStance("down");
@@ -31,8 +31,7 @@ GunWeapon::GunWeapon(const std::string &name, TileCharacter *tileCharacter, jl::
 		pushFrame(sf::IntRect(32,32,16,16), 0.01).
 		pushFrame(sf::IntRect(48,32,16,16), 0.01).
 		pushFrame(sf::IntRect(64,32,16,16), 0.01).
-		pushFrame(sf::IntRect(80,32,16,16), 0.01).
-		pushFrame(sf::IntRect(96,32,16,16), 0.01);
+		pushFrame(sf::IntRect(80,32,16,16), 0.01);
 	addBulletFireAnimation("bulletFire");
 
 	setBulletAnimation(anim);
